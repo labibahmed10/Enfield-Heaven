23a0a8c34a8b083dcd49137ac54e49c4aac90b80
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-26 00:26:47 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Enfield Heaven</h1>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
